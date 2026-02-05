a5ef433294c21a10221a670c1c5e158ea2b5acb8
TheJokes1 <59894478+TheJokes1@users.noreply.github.com>
2026-02-05 15:48:27 +0100

    Refactor database connection setup in db.js

--- db.js
+++ db.js
@@ -1,21 +1,8 @@
-// db.js
 const { Pool } = require("pg");
-const dns = require("dns");
-
-// Hard-force IPv4 only for all connections made by 'pg'
-const ipv4Lookup = (hostname, options, callback) => {
-  return dns.lookup(hostname, { ...options, family: 4, all: false }, callback);
-};
-
-const connectionString = process.env.DATABASE_URL;
 
 const pool = new Pool({
-  connectionString,
-  ssl: { rejectUnauthorized: false }, // keep SSL on for Supabase/managed DBs
-  lookup: ipv4Lookup,                 // <-- this is the key line
-  // Optional tuning:
-  // max: 5,
-  // idleTimeoutMillis: 10000,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = {
